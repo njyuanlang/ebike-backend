2cacae059dd697db39dd5d4c30ab3aa60e3a773b
Guan Bo <guanbo2002@gmail.com>
2015-04-16 17:10:39 +0800

    Add: test statistic

--- common/models/test.js
+++ common/models/test.js
@@ -11,4 +11,44 @@ module.exports = function(Test) {
     next()
   })  
 
+  Test.stat = function (filter, next) {
+    var Model = Test
+    filter.where = Model._coerce(filter.where)
+    var connector = Model.getDataSource().connector
+    filter.where = connector.buildWhere(Model.modelName, filter.where)
+    var collection = connector.collection(Model.modelName)
+    collection.aggregate([
+      {
+        $match: filter.where
+      },
+      {
+        $group: {
+          _id: {year: {$year: "$created"}, month: {$month: "$created"}, dayOfMonth: {$dayOfMonth: "$created"}},
+          count: {$sum: 1}
+        }
+      },
+      {
+        $sort: {
+          _id: 1
+        }
+      }
+    ],function (err, results) {
+      if(err) {
+        next(err)
+      } else {
+        next(null, results)
+      }
+    })    
+  }
+  
+  Test.remoteMethod(
+    'stat',
+    {
+      accepts: [
+        {arg:'filter', type: 'Object', http: {source: 'query'}, root:true}
+      ],
+      returns: {arg:'data', type: 'Array', root: true},
+      http: {verb: 'get'}
+    }
+  )
 };
