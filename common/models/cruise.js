303f27f0b6228667f4e51e1b710fec73b920e496
Guan Bo <guanbo2002@gmail.com>
2015-04-24 11:50:51 +0800

    Add: cruise bike info

--- common/models/cruise.js
+++ common/models/cruise.js
@@ -1,5 +1,18 @@
 module.exports = function(Cruise) {
 
+  Cruise.beforeRemote('create', function (ctx, unused, next) {
+    if(ctx.req.body.bikeId) {
+      Cruise.app.models.Bike.findById(ctx.req.body.bikeId, function (err, bike) {
+        if(err) return next(err)
+        ctx.req.body.bike = bike
+        delete ctx.req.body.bikeId
+        next()
+      })
+    } else {
+      next(new Error('no bikeId'))
+    }
+  })
+
   Cruise.observe('before save', function timeStamp(ctx, next) {
     var now = new Date()
     if(ctx.instance) {
