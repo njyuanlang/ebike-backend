e6290854063549f2a1a6ed61d69b603b332cb25a
Guan Bo <guanbo2002@gmail.com>
2015-05-12 18:21:58 +0800

    Add: upload

---
+++ common/models/upload.js
@@ -0,0 +1,15 @@
+module.exports = function(Upload) {
+  
+  // Upload.beforeRemote("*", function (ctx, unused, next) {
+  //   console.log(ctx.req.headers, ctx.req.params)
+  //
+  //   if(ctx.req.body.search(reg) != -1) {
+  //     console.log('Found base64')
+  //     ctx.req.body = ctx.req.body.replace(reg, "")
+  //     ctx.req.body = ctx.req.body.replace(/ /g, '+')
+  //     ctx.req.body = new Buffer(ctx.req.body, 'base64')
+  //   }
+  //   next()
+  // })
+  
+};
