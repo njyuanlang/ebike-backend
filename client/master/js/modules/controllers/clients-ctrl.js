562c45b09ee34939f4fc5135ac2da7a7596794f6
Guan Bo <guanbo2002@gmail.com>
2015-11-24 18:33:59 +0800

    Fixed: user export

--- client/master/js/modules/controllers/clients-ctrl.js
+++ client/master/js/modules/controllers/clients-ctrl.js
@@ -16,7 +16,7 @@ App.controller('ClientsController', function ($scope, $state, User, ngTableParam
       opt.skip = (params.page()-1)*opt.limit
       opt.where = {realm: "client"}
       if($scope.filter.text != '') {
-        opt.where.username = {like: $scope.filter.text}
+        opt.where['username'] = {regex: $scope.filter.text}
       }
       User.count({where: opt.where}, function (result) {
         $scope.tableParams.total(result.count)
@@ -38,5 +38,27 @@ App.controller('ClientsController', function ($scope, $state, User, ngTableParam
       touser: user
     }
     $state.go('app.message-compose');
+  }
+  
+  $scope.generate = function () {
+    $http.get(urlBase+'/users/export?access_token='+LoopBackAuth.accessTokenId, {
+      responseType: 'arraybuffer'
+    })
+      .success(function (data, status, headers, config) {
+        var blob = new Blob([data], {
+          type: 'text/csv;charset=GBK;'
+        });
+        var downloadContainer = angular.element('<div data-tap-disabled="true"><a></a></div>');
+        var downloadLink = angular.element(downloadContainer.children()[0]);
+        downloadLink.attr('href', window.URL.createObjectURL(blob));
+        downloadLink.attr('download', $scope.user.email+Date.now()+'.csv');
+        downloadLink.attr('target', '_blank');
+
+        $document.find('body').append(downloadContainer);
+        $timeout(function () {
+          downloadLink[0].click();
+          downloadLink.remove();
+        }, null);
+      });
   }
 })
