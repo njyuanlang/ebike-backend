1824fa8ed25fc926ee63d70257839ab181dedcce
Guan Bo <guanbo2002@gmail.com>
2017-02-07 23:50:55 +0800

    Add: docker startup

---
+++ startup.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+HOST=api.baoxu360.com
+CONTAINER=backend
+VPATH=$(pwd)/conf.d/:/etc/nginx/conf.d
+if [[ $1 == 'manufacturer' ]]; then
+  HOST=cs.baoxu360.com
+  CONTAINER=manufacturer
+  VPATH=$(pwd)/../ebike-manufacturer/:/usr/share/nginx/html:ro
+fi
+
+docker rm -f $CONTAINER
+docker run -d \
+    --name $CONTAINER\
+    -e "VIRTUAL_HOST="$HOST \
+    -e "VIRTUAL_NETWORK=nginx-proxy" \
+    -e "VIRTUAL_PORT=80" \
+    -e "LETSENCRYPT_HOST=api.baoxu360.com" \
+    -e "LETSENCRYPT_EMAIL=guanbo2002@gmail.com" \
+    --network nginx-proxy \
+    -v $VPATH \
+    nginx
